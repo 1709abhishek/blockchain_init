47992c11fff51c9f3ab9f4c4b251a77ee3ffda6d
1709abhishek <1709abhishek@gmail.com>
2025-04-01 20:15:25 -0400

    add my first smart contract

---
+++ contracts/3_Ballot.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.4.17;
+
+contract Inbox {
+    string public message;
+
+    function Inbox(string initialMessage) public {
+        message = initialMessage;
+    }
+
+    function setMessage(string newMessage) public {
+        message = newMessage;
+    }
+
+    function doMath(int a, int b){
+        a+b;
+        a*b;
+        a/b;
+        a-b;
+        a==b;
+        a<=b;
+        a>=b;
+        a != b;
+      }
+}
